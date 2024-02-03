4f0165f1f6552dfc4c3b8254dfa0a7a4905c87d2
shabanii2023F <shabanic@aol.com>
2024-02-03 14:05:55 -0500

    more scraper part, removed image, can scan everything else now.

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -4,10 +4,24 @@ import requests
 class Scraper:
     def __init__(self) -> None:
         pass
-    def read(self, website):
+
+    def readLender(self, website):
         page = requests.get(website)
         soup = BeautifulSoup(page.text, "html.parser")
-        loans = soup.findAll('p', attrs={'class':'ct_rb fv_R6'})
-        
-        for loan in loans:
-            print(loan.text)
+        lenders = soup.findAll('p', attrs={'class':'ct_rb fv_R6'})
+        for lender in lenders:
+            print(lender.text)
+    
+    def readMaxLoanAmount(self, website):
+        page = requests.get(website)
+        soup = BeautifulSoup(page.text, "html.parser")
+        loanAmts = soup.findAll('td', attrs={'class':'h7_jf h7_Wp'})
+        for loans in loanAmts:
+            print(loans.text)
+    
+    def readMinCreditScore(self, website):
+        page = requests.get(website)
+        soup = BeautifulSoup(page.text, "html.parser")
+        scores = soup.findAll('p', attrs={'class':'ct_rb fv_R6'})
+        for score in scores:
+            print(score.text)
--- webscraper/scraper_test.py
+++ webscraper/scraper_test.py
@@ -1,4 +1,4 @@
 from scraper import Scraper
 
 reader = Scraper()
-reader.read("https://www.nerdwallet.com/best/small-business/unsecured-business-loans")
+reader.readMaxLoanAmount("https://www.nerdwallet.com/best/small-business/unsecured-business-loans")
