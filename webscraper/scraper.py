348e3ea67c784399579160b659ef6c3df8783183
shabanii2023F <shabanic@aol.com>
2024-02-03 15:45:22 -0500

    trying to create json file but it dont work

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 class Scraper:
     def __init__(self) -> None:
@@ -13,7 +14,27 @@ class Scraper:
         for lender in lenders:
             lenderDoc.write(lender.text)
             lenderDoc.write("\n")
-    
+        
+        # convert txt into json now
+        jsondict = {}
+        fields = ["lender"]
+        with open("lenders.txt") as ld:
+            lenderID = 1
+            for line in ld:
+                desc = list(line.strip("\n").split(None, 4))
+                print(desc)
+                key = "lender" + str(lenderID)
+                i = 0
+                newDict = {}
+                while i < len(fields):
+                    newDict[fields[i]] = desc[i]
+                    i += 1
+                jsondict[key] = newDict
+                lenderID += 1
+        finaljson = open("lenders.json", "w")
+        json.dump(jsondict, finaljson, indent = 4)
+        finaljson.close()    
+
     def readLoanInfo(self, website):
         page = requests.get(website)
         soup = BeautifulSoup(page.text, "html.parser")
@@ -22,5 +43,4 @@ class Scraper:
         for loans in loanInfo:
             loanInfoDoc.write(loans.text)
             loanInfoDoc.write("\n")
-            print(loans.text)
     
--- webscraper/scraper_test.py
+++ webscraper/scraper_test.py
@@ -2,4 +2,4 @@ from scraper import Scraper
 
 reader = Scraper()
 reader.readLender("https://www.nerdwallet.com/best/small-business/unsecured-business-loans")
-reader.readLoanInfo("https://www.nerdwallet.com/best/small-business/unsecured-business-loans")
+# reader.readLoanInfo("https://www.nerdwallet.com/best/small-business/unsecured-business-loans")
