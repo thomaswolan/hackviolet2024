66d679ac821cee8987cc00d8f23536fc6e49ed29
Peter Do <pdoo2004@vt.edu>
2024-02-03 12:56:58 -0500

    Changes

---
+++ hackvioletproject2024/src/components/Banner.js
@@ -0,0 +1,79 @@
+import { useState, useEffect, useCallback, useMemo } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/new-header.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(100 - Math.random() * 100);
+  const period = 2000;
+
+  const toRotate = useMemo(() => ["Web Developer", "Software Engineer", "Virginia Tech"], []);
+
+  const tick = useCallback(() => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum((prevLoopNum) => prevLoopNum + 1);
+      setDelta(500);
+    }
+  }, [loopNum, toRotate, isDeleting, text, setDelta, setIsDeleting, setLoopNum]);
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [tick, delta]);
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h1>{`Thomas Wolan`}</h1>
+                  <h2 className="txt-rotate" data-period="1000" data-rotate='[ "Web Developer", "Software Engineer", "Virginia Tech" ]'>
+                    <span className="wrap">{text}</span>
+                  </h2>
+                  <p></p>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
