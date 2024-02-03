0676f8d61ddd4372f42f856a8f5d36e5209462d9
twolan34 <twolan34@vt.edu>
2024-02-03 14:24:28 -0500

    logo

--- hackvioletproject2024/src/components/Navbar.js
+++ hackvioletproject2024/src/components/Navbar.js
@@ -34,7 +34,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            
+            <img src={logo} alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
