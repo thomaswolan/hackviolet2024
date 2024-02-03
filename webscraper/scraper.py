11799c6e53b6d320260f8b11e59bb27ff56716a8
shabanii2023F <shabanic@aol.com>
2024-02-03 14:46:46 -0500

    gonna work with error on scraper for now tbh

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -5,23 +5,13 @@ class Scraper:
     def __init__(self) -> None:
         pass
 
-    def readLender(self, website):
+    def readInfo(self, website):
         page = requests.get(website)
         soup = BeautifulSoup(page.text, "html.parser")
-        lenders = soup.findAll('p', attrs={'class':'ct_rb fv_R6'})
-        for lender in lenders:
-            print(lender.text)
-    
-    def readMaxLoanAmount(self, website):
-        page = requests.get(website)
-        soup = BeautifulSoup(page.text, "html.parser")
-        loanAmts = soup.findAll('td', attrs={'class':'h7_jf h7_Wp'})
-        for loans in loanAmts:
+        loanInfo = soup.findAll('td', attrs={'class':'h7_jf h7_Wp'})
+        loanInfoDoc = open("loanAmts.txt", "w")
+        for loans in loanInfo:
+            loanInfoDoc.write(loans.text)
+            loanInfoDoc.write("\n")
             print(loans.text)
     
-    def readMinCreditScore(self, website):
-        page = requests.get(website)
-        soup = BeautifulSoup(page.text, "html.parser")
-        scores = soup.findAll('p', attrs={'class':'ct_rb fv_R6'})
-        for score in scores:
-            print(score.text)
